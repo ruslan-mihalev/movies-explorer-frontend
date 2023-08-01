63fdd9b65f2883866777daf86ff097db0f1b91e0
Ruslan <ruslan.mihalev@gmail.com>
2023-08-01 14:37:55 +1200

    Created stub classes for Field and Form components

---
+++ src/components/Field/Field.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Field.css';
+
+function Field() {
+    return (<div className="field"></div>);
+}
+
+export default Field;
---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Form.css';
+
+function Form() {
+    return (<div className="form"></div>);
+}
+
+export default Form;
