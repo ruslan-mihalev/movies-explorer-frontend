e1368872ed5619a38197a7595ec92d70d8e2c678
Ruslan <ruslan.mihalev@gmail.com>
2023-08-02 16:20:30 +1200

    Created logo component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 import './Header.css';
+import Navigation from "../Navigation/Navigation";
+import Logo from "../Logo/Logo";
 
 function Header() {
-    return (<header className='header'></header>);
+    return (
+        <header className='header'>
+            <Logo />
+            <Navigation />
+        </header>
+    );
 }
 
 export default Header;
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,9 @@ import React from 'react';
 import './Navigation.css';
 
 function Navigation() {
-    return (<div className="navigation"></div>);
+    return (
+        <div className='navigation'></div>
+    );
 }
 
 export default Navigation;
