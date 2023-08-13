1bbd16b1da672a30ce7c02f6971e9590f4e40650
Ruslan <ruslan.mihalev@gmail.com>
2023-08-14 08:07:27 +1200

    Added MovieCard click listener

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 import './MoviesCard.css';
 
-function MoviesCard({ name, duration, imgUrl, actionType }) {
+function MoviesCard({ name, duration, imgUrl, actionType, onCardClick, onActionClick }) {
 
     const actionModifier = actionType === 'remove' ? 'movies-card__action_type_remove' : (actionType === 'saved' ? 'movies-card__action_type_saved' : 'movies-card__action_type_default');
     const actionClass = `movies-card__action ${actionModifier}`;
 
     return (
         <article className='movies-card'>
-            <div className='movies-card__image-container'>
+            <div className='movies-card__image-container' onClick={onCardClick}>
                 <img className='movies-card__image' alt={name} src={imgUrl}/>
             </div>
             <div className='movies-card__name-container'>
                 <h3 className='movies-card__name'>{name}</h3>
-                <button className={actionClass}/>
+                <button className={actionClass} onClick={onActionClick}/>
                 <p className='movies-card__duration'>{duration}</p>
             </div>
         </article>
