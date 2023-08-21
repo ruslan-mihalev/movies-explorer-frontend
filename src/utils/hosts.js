5c657eb5d449645a641b9fef4484111aac8bb4f6
Ruslan <ruslan.mihalev@gmail.com>
2023-08-22 03:38:54 +1200

    Corrected api host

--- src/utils/hosts.js
+++ src/utils/hosts.js
@@ -1,3 +1,2 @@
-// export const BASE_API_URL = 'http://localhost:3030'; // For degugging only
-export const BASE_API_URL = 'https://kinopoisk.nomoredomains.xyz';
+export const BASE_API_URL = 'https://api.kinopoisk.nomoredomains.xyz';
 export const BASE_MOVIES_API_URL = 'https://api.nomoreparties.co';
