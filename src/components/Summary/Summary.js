765647163c663598abcb9cd0153b2ca607bb7a99
Ruslan <ruslan.mihalev@gmail.com>
2023-08-22 02:50:33 +1200

    Fixed summary a bit

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -11,14 +11,10 @@ function Summary({ className: mixinClass }) {
         <div className={className}>
             <div className="summary__info-container">
                 <div className='summary__personal-info-container'>
-                    <p className='summary__name'>Виталий</p>
-                    <p className='summary__brief-info'>Фронтенд-разработчик, 30 лет</p>
-                    <SimpleText className='summary__detail-info'>Я родился и живу в Саратове, закончил факультет
-                        экономики
-                        СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал
-                        кодить. С
-                        2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал
-                        заниматься фриланс-заказами и ушёл с постоянной работы.</SimpleText>
+                    <p className='summary__name'>Руслан</p>
+                    <p className='summary__brief-info'>Фронтенд-разработчик, 34 лет</p>
+                    <SimpleText className='summary__detail-info'>Учился в Казани. Начинал карьеру там-же. После переехал в Москву. Начинал как мобильный разработчик, сейчас плавно перехожу в фулстек.
+                       </SimpleText>
                 </div>
                 <Link className='summary__social-link' to='https://github.com/ruslan-mihalev' target='_blank'>Github</Link>
             </div>
